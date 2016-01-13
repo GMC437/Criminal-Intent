b87bbc1592ccde7c6d290dd7cbcf13dd7575cdf7
Grant <Pokeamn43>
2016-01-13 14:47:59 +0000

    Chapter 11 is finished. Added View pager to the Crime Fragment.

--- app/src/main/java/com/wrightcontrol/criminalintent/app/controller/crime/CrimeActivity.java
+++ app/src/main/java/com/wrightcontrol/criminalintent/app/controller/crime/CrimeActivity.java
@@ -1,16 +1,8 @@
 package com.wrightcontrol.criminalintent.app.controller.crime;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.Fragment;
-import com.wrightcontrol.criminalintent.app.R;
-import com.wrightcontrol.criminalintent.app.controller.abstraction.SingleFragmentActivity;
-
-import java.util.UUID;
-
-public class CrimeActivity extends SingleFragmentActivity {
-
-    private static final String EXTRA_CRIME_ID = "com.wrightcontrol.android.criminalintent.app.crime_id";
+public class CrimeActivity  {
+/*
+    private static final String EXTRA_CRIME_ID = "com.wrightcontrol.criminalintent.app.crime_id";
 
     @Override
     protected int getLayout() {
@@ -33,5 +25,5 @@ public class CrimeActivity extends SingleFragmentActivity {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
         intent.putExtra(EXTRA_CRIME_ID, crimeId);
         return intent;
-    }
+    }*/
 }
---
+++ app/src/main/java/com/wrightcontrol/criminalintent/app/controller/crimePager/CrimePagerActivity.java
@@ -0,0 +1,69 @@
+package com.wrightcontrol.criminalintent.app.controller.crimePager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import com.wrightcontrol.criminalintent.app.R;
+import com.wrightcontrol.criminalintent.app.controller.crime.CrimeFragment;
+import com.wrightcontrol.criminalintent.app.model.Crime;
+import com.wrightcontrol.criminalintent.app.model.CrimeLab;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Grant on 13/01/2016.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+
+    private static final String EXTRA_CRIME_ID = "com.wrightcontrol.criminalintent.app.crime_id";
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+        instantiateWidgets();
+    }
+
+    private void instantiateWidgets(){
+        UUID crimeId = (UUID)getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        for(int i = 0; i < mCrimes.size(); i++){
+            if (mCrimes.get(i).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+    }
+}
