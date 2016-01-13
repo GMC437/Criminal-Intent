c3acf6715732a8b1fd028f82c8e4337393c872ca
Grant <Pokeamn43>
2016-01-13 12:40:58 +0000

    Chapter 10 is finished. Added Intent to Crime Fragment.

--- app/src/main/java/com/wrightcontrol/criminalintent/app/controller/crime/CrimeActivity.java
+++ app/src/main/java/com/wrightcontrol/criminalintent/app/controller/crime/CrimeActivity.java
@@ -1,10 +1,16 @@
 package com.wrightcontrol.criminalintent.app.controller.crime;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import com.wrightcontrol.criminalintent.app.R;
 import com.wrightcontrol.criminalintent.app.controller.abstraction.SingleFragmentActivity;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    private static final String EXTRA_CRIME_ID = "com.wrightcontrol.android.criminalintent.app.crime_id";
 
     @Override
     protected int getLayout() {
@@ -18,8 +24,14 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 
-
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 }
--- app/src/main/java/com/wrightcontrol/criminalintent/app/controller/crimeList/CrimeListFragment.java
+++ app/src/main/java/com/wrightcontrol/criminalintent/app/controller/crimeList/CrimeListFragment.java
@@ -1,7 +1,7 @@
 package com.wrightcontrol.criminalintent.app.controller.crimeList;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,9 +11,9 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import com.wrightcontrol.criminalintent.app.R;
+import com.wrightcontrol.criminalintent.app.controller.crime.CrimeActivity;
 import com.wrightcontrol.criminalintent.app.model.Crime;
 import com.wrightcontrol.criminalintent.app.model.CrimeLab;
-import com.wrightcontrol.criminalintent.app.utils.ToastAlert;
 
 import java.util.List;
 
@@ -22,10 +22,11 @@ import java.util.List;
  */
 public class CrimeListFragment extends Fragment {
 
+    // widgets vars
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
 
-    @Nullable
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
@@ -34,27 +35,40 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
+    // Instantiates Widgets
     private void instantiateWidgets(View v) {
         mCrimeRecyclerView = (RecyclerView) v.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
+    // Takes the crimes list and assigns to adapter. That the Recycler view has its needed information.
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if(mAdapter == null){
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        }else{
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
+    // Adapter for the Recycler view
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>  {
         private List<Crime> mCrimes;
-
 
         public CrimeAdapter(List<Crime> crimes) {
             mCrimes = crimes;
         }
 
+        // Creates the view for the Recycler view to be displayed.
         @Override
         public CrimeHolder onCreateViewHolder(ViewGroup parent, int i) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
@@ -62,12 +76,14 @@ public class CrimeListFragment extends Fragment {
             return new CrimeHolder(view);
         }
 
+        // This binds the list to the layout
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position) {
             Crime crime = mCrimes.get(position);
             holder.bindCrime(crime);
         }
 
+        // Gets the count of how many rows are in the list
         @Override
         public int getItemCount() {
             return mCrimes.size();
@@ -75,12 +91,14 @@ public class CrimeListFragment extends Fragment {
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+       // Widgets vars
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private CheckBox mSolvedCheckBox;
-
+        // Crime class obj
         private Crime mCrime;
 
+        // Assigns clicks to the views. Assigns the correct widget to the correct id.
         public CrimeHolder(View itemView) {
             super(itemView);
 
@@ -91,6 +109,7 @@ public class CrimeListFragment extends Fragment {
             mSolvedCheckBox = (CheckBox) itemView.findViewById(R.id.list_item_crime_solved_check_box);
         }
 
+        // Binds what the widget will display when using the app.
         public void bindCrime(Crime crime){
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
@@ -98,9 +117,11 @@ public class CrimeListFragment extends Fragment {
             mSolvedCheckBox.setChecked(mCrime.getSolved());
         }
 
+        // Shows a toast message when a row is clicked.
         @Override
         public void onClick(View v) {
-            ToastAlert.showShortToast(getActivity(), mCrime.getTitle());
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
         }
     }
 }
